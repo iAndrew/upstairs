9c20662d3a3d494c27a3070adff389784f4409af
Andrew Lebedev <andrew.devel@gmail.com>
2010-10-27 23:23:18 +0700

    added tests for users# and sessions# controllers

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -13,5 +13,16 @@ describe UsersController do
       get :new
       response.should have_selector("title", :content => "Sign up")
     end
+    
+    it "should have an email field" do
+      get :new
+      response.should have_selector("input[name='user[email]'][type='email']")            
+    end
+    
+    it "should have a password field" do
+      get :new
+      response.should have_selector("input[name='user[password]'][type='password']")
+    end
   end
+
 end
