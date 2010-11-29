86f6561732a2d91cff5dda026dc992802d5ff96f
Karbaman <peter.lazarev@gmail.com>
2010-11-29 22:52:28 +0600

    edit user in place feature was added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,15 @@
 class UsersController < ApplicationController
   before_filter :authenticate, :except => [:new, :create]
-  
+
   def new
     @title = "Sign up"
     @user = User.new
     if (session[:omniauth])
       @user.apply_omniauth(session[:omniauth])
-      @user.valid?  
+      @user.valid?
     end
   end
-  
+
   def create
     @user = User.new(params[:user])
     if (session[:omniauth])
@@ -17,7 +17,7 @@ class UsersController < ApplicationController
     end
     if @user.save
       session[:omniauth] = nil
-      sign_in @user    
+      sign_in @user
       if params[:user][:avatar].blank?
         redirect_to @user
       else
@@ -26,37 +26,50 @@ class UsersController < ApplicationController
     else
       @title = "Sign up"
       render :new
-    end    
+    end
   end
-  
+
   def edit
     @title = "Edit profile"
     @user = User.find(params[:id])
   end
-  
+
   def update
     @user = User.find(params[:id])
-    if @user.update_attributes(params[:user])
-      if params[:user][:avatar].blank?
-        redirect_to @user
-      else
-        @title = "Avatar cropping"
-        redirect_to avatar_cropping_user_url(@user)
-      end
-    else
-      @title = "Edit profile"
-      render :edit
+    respond_to do |format|
+      format.html {
+        if @user.update_attributes(params[:user])
+          if params[:user][:avatar].blank?
+            redirect_to @user
+          else
+            @title = "Avatar cropping"
+            redirect_to avatar_cropping_user_url(@user)
+          end
+        else
+          @title = "Edit profile"
+          render :edit
+        end
+      }
+      format.json {
+        @user.first_name = params[:first_name]
+        @user.second_name = params[:second_name]
+        if @user.save()
+          render :json => {:first_name => @user.first_name, :second_name => @user.second_name}
+        else
+          render :text => "cannot save"
+        end
+      }
     end
   end
-  
+
   def avatar_cropping
-   @title = "Avatar cropping"
-   @user = User.find(params[:id])
-   unless @user.avatar.exists?
-     redirect_to edit_user_url(@user)
-   end
+    @title = "Avatar cropping"
+    @user = User.find(params[:id])
+    unless @user.avatar.exists?
+      redirect_to edit_user_url(@user)
+    end
   end
-  
+
   def show
     @user = User.find(params[:id])
     @title = @user.email
